322252b5c05a6105c0d99274a2f78f9b077ee4fc
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2021-03-25 02:05:29 +0200

    Updates

---
+++ static/js/home-view.0c3dac04.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[1],{106:function(e,t,o){e.exports={container:"HomeView_container__y_5dT",title:"HomeView_title__2AeeH"}},114:function(e,t,o){"use strict";o.r(t);o(0);var n=o(106),i=o.n(n),c=o(1);t.default=function(){return Object(c.jsx)("div",{className:i.a.container,children:Object(c.jsx)("h1",{className:i.a.title,children:"The PhoneBook"})})}}}]);
+//# sourceMappingURL=home-view.0c3dac04.chunk.js.map
